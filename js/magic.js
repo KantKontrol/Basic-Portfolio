3908051462318f7280a48c9c61198a5f0cc3d038
Lord Vern <nick.derissio.tech@gmail.com>
2019-12-16 08:29:55 -0500

    setup buttons with js and updated buttons in all HTML docs

---
+++ js/magic.js
@@ -0,0 +1,13 @@
+
+
+function toAbout() {
+    window.location.href = "index.html";
+}
+
+function toContact() {
+    window.location.href = "contact.html";
+}
+
+function toPortfolio() {
+    window.location.href = "portfolio.html";
+}
